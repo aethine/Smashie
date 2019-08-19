0ca2a57c2b3f05fb28c53f44752ab69578a5ca85
aethine <yepicness@gmail.com>
2019-08-18 20:36:51 -0400

    Just testing out github with unity

---
+++ Smashie/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    public Rigidbody RigidBody;
+    public float Speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 force = new Vector3(Input.GetAxis("Horizontal") * Speed, 0f, Input.GetAxis("Vertical") * Speed);
+        RigidBody.AddForce(force);
+    }
+}
